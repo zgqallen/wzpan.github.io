3c8e6d6755b04fa3c1adc98073fc9c4c24f54b7c
Joseph <m@hahack.com>
2017-09-03 13:12:54 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -279,7 +279,7 @@ var _getRecentIssues = function _getRecentIssues(params, callback) {
         data: client_id && client_secret ? "client_id=" + client_id + "&client_secret=" + client_secret : '',
         success: function success(issues) {
             if (issues.length > count) {
-                if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || navigator.userAgent.indexOf("Safari") != -1 || !!document.documentMode == true) {
+                if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) || !!document.documentMode == true) {
                     issues = issues.sort(CompareDate).slice(0, 5);
                 } else {
                     issues = issues.sort(CompareDate).reverse().slice(0, 5);
@@ -308,7 +308,7 @@ var _getRecentComments = function _getRecentComments(params, callback) {
         data: client_id && client_secret ? "client_id=" + client_id + "&client_secret=" + client_secret : '',
         success: function success(comments) {
             if (comments.length > count) {
-                if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || navigator.userAgent.indexOf("Safari") != -1 || !!document.documentMode == true) {
+                if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) || !!document.documentMode == true) {
                     comments = comments.sort(CompareDate).slice(0, 5);
                 } else {
                     comments = comments.sort(CompareDate).reverse().slice(0, 5);
@@ -344,7 +344,7 @@ var getRecentCommentsList = function getRecentCommentsList(params) {
         recentList = recentList.concat(issues);
         _getRecentComments(params, function (comments) {
             recentList = recentList.concat(comments);
-            if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || navigator.userAgent.indexOf("Safari") != -1 || !!document.documentMode == true) {
+            if (navigator.userAgent.indexOf("MSIE") != -1 || navigator.userAgent.indexOf("Edge") != -1 || (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) || !!document.documentMode == true) {
                 recentList = recentList.sort(CompareDate);
             } else {
                 recentList = recentList.sort(CompareDate).reverse();
